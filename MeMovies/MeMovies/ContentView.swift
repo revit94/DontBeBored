2e41d1d9802ea7a25cdd7f9c15a7b3891a3fcc91
reztsov <reztsov.vitaliy@gmail.com>
2019-07-16 12:29:36 +0300

    created project

---
+++ MeMovies/MeMovies/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MeMovies
+//
+//  Created by reztsov on 7/16/19.
+//  Copyright © 2019 reztsov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
